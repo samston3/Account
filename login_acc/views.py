b74079c19f733fd66eb3b8e0644356017550bc62
Samston christo dass <samston23@gmail.com>
2025-08-02 21:53:50 +0530

    Update views.py

--- login_acc/views.py
+++ login_acc/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 
+
+def home(request):
+    return render(request, 'home.html')
+    
 def login_view(request):
     if request.method == 'POST':
         username = request.POST['username']
