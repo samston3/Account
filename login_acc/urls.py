d46bb4c4532a502b3b311a7fa90b96bfd6ab9b09
Samston christo dass <samston23@gmail.com>
2025-08-02 22:26:40 +0530

    Update urls.py

--- login_acc/urls.py
+++ login_acc/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('login/', views.login_view, name='login'),
+    path('templates/login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
-    path('register/', views.register_view, name='register'),
+    path('templates/register/', views.register_view, name='register'),
 ]
